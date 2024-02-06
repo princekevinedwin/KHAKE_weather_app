a487e50fd4c843b247181510ef424bb848a55cc5
Honourfemi <honour.oluwafemi@binghamuni.edu.ng>
2024-02-06 00:05:24 -0800

    added open_main_window

--- WEATHER.py
+++ WEATHER.py
@@ -1,6 +1,7 @@
 import requests
 from tkinter import messagebox
-
+import tkinter as tk
+from PIL import Image, ImageTk
 
 def get_weather():
     global city_entry, result_text
@@ -28,3 +29,22 @@ def get_weather():
             f" {header_text}\n WEATHER: {weather}\n TEMPERATURE: {temp}°F\n COORDINATE: {coord}\n PRESSURE: {pressure}hPa\n HUMIDITY: {humidity}%\n COUNTRY: {sys}")
 
 
+
+# Make city_entry and result_text global variables
+city_entry = None
+result_text = None
+
+def open_main_window():
+    global city_entry, result_text
+    cover_page.destroy()
+    main_window = tk.Tk()
+    main_window.geometry("767x547")
+    main_window.title("K.H.A.K.E weather app")
+
+    # Pillow
+    image_path = r"C:\Users\user pc\Pictures\Screenshot 2024-01-28 235308.png"
+    pil_image = Image.open(image_path)
+    background_image = ImageTk.PhotoImage(pil_image)
+
+    background_label = tk.Label(main_window, image=background_image)
+    background_label.place(relwidth=1, relheight=1)
